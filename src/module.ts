451a8170b82643954f378e7459d5eed0e4857a34
clenz <christian.lenz@jambit.com>
2024-10-09 14:31:53 +0200

    #5 - Change dir path loading

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,28 @@
-import { defineNuxtModule, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, createResolver } from "@nuxt/kit";
+import { fileURLToPath } from "url";
+import { dirname, resolve as pathResolve } from "path";
 
 export default defineNuxtModule({
   setup(_options, nuxt) {
+    // Get the directory of the module file
+    const moduleDir = dirname(fileURLToPath(import.meta.url));
+
     // Create resolver to resolve relative paths
-    // TODO: A new @nuxt/content transformer is also just a nuxt-module
-    // TODO: Missing in the @nuxt/content doc
-    const { resolve } = createResolver(import.meta.url)
+    const { resolve } = createResolver(moduleDir);
 
-    nuxt.options.nitro.externals = nuxt.options.nitro.externals || {}
-    nuxt.options.nitro.externals.inline = nuxt.options.nitro.externals.inline || []
-    nuxt.options.nitro.externals.inline.push(resolve('./module'))
+    // Ensure Nitro externals are configured correctly
+    nuxt.options.nitro.externals = nuxt.options.nitro.externals || {};
+    nuxt.options.nitro.externals.inline =
+      nuxt.options.nitro.externals.inline || [];
+    nuxt.options.nitro.externals.inline.push(resolve("./module"));
 
+    // Convert the resolved path to an absolute path for the plugin
+    const absolutePluginPath = pathResolve(moduleDir, "./runtime/plugin.ts");
+
+    // Register the plugin in the Nuxt content hook
     // @ts-ignore
-    nuxt.hook('content:context', (contentContext) => {
-      contentContext.transformers.push(resolve('./runtime/plugin.ts'))
-    })
-  }
-})
+    nuxt.hook("content:context", (contentContext) => {
+      contentContext.transformers.push(absolutePluginPath);
+    });
+  },
+});
